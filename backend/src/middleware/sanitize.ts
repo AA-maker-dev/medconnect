cf567011b5aab617efb8b8bf8bc7cf8d505ff6c1
JITENDRARAUT <jitendraraut903@gmail.com>
2026-07-24 18:12:06 +0545

    Update sanitize.ts

--- backend/src/middleware/sanitize.ts
+++ backend/src/middleware/sanitize.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';;
 import xss from 'xss';
 
 function sanitizeValue(value: unknown): unknown {
