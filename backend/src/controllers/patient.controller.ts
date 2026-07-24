fcee81e758729684e8ea15f4df91acf1f706da93
JITENDRARAUT <jitendraraut903@gmail.com>
2026-07-24 18:08:57 +0545

    Update patient.controller.ts

--- backend/src/controllers/patient.controller.ts
+++ backend/src/controllers/patient.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { asyncHandler } from '../utils/asyncHandler';;
+import { asyncHandler } from '../utils/asyncHandler';
 import { sendSuccess } from '../utils/ApiResponse';
 import * as patientService from '../services/patient.service';
 
