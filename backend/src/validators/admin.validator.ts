2829046c4848ae0ab603822f5b0d070977a593fc
JITENDRARAUT <jitendraraut903@gmail.com>
2026-07-25 10:05:14 +0545

    Update admin.validator.ts

--- backend/src/validators/admin.validator.ts
+++ backend/src/validators/admin.validator.ts
@@ -5,7 +5,7 @@ export const paginationQuerySchema = z.object({
     page: z.coerce.number().int().min(1).optional().default(1),
     limit: z.coerce.number().int().min(1).max(100).optional().default(10),
   }),
-});
+}); 
 
 export const listPatientsQuerySchema = z.object({
   query: z.object({
