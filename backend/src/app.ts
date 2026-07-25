952800e804c24ca514203a15cf5c6bda85926efa
JITENDRARAUT <jitendraraut903@gmail.com>
2026-07-25 10:06:08 +0545

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import helmet from 'helmet';
+import helmet from 'helmet'; 
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { env } from './config/env';
